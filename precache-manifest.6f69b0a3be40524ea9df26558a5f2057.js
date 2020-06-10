82d345263d14584e13daaa23f6a2019e9f126bde
alem-alem <boricalem@gmail.com>
2020-06-10 13:07:29 +0200

    Updates

---
+++ precache-manifest.6f69b0a3be40524ea9df26558a5f2057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53b53618b6012cb9207079c5d24bd40",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "2c9ee95505281afa0df7",
+    "url": "/react-todo-app/static/css/main.7ebe3e7f.chunk.css"
+  },
+  {
+    "revision": "3b42651380bbb35bd0f9",
+    "url": "/react-todo-app/static/js/2.1775ecea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.1775ecea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9ee95505281afa0df7",
+    "url": "/react-todo-app/static/js/main.4ae5be55.chunk.js"
+  },
+  {
+    "revision": "e2be8d621a4d2ec1bc12",
+    "url": "/react-todo-app/static/js/runtime-main.42e3d57b.js"
+  }
+]);
